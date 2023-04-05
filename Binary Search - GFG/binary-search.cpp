2e28c23e1c74251f915e976d0280964578d86c41
Nishant Raj <112903677+nishantthedevill@users.noreply.github.com>
2023-04-05 11:31:09 +0530

    Added solution - LeetHub

--- Binary Search - GFG/binary-search.cpp
+++ Binary Search - GFG/binary-search.cpp
@@ -11,8 +11,7 @@ using namespace std;
 class Solution {
   public:
     int binarysearch(int arr[], int n, int k) {
-        // code here
-        //int n = arr.size();
+        
         int low=0;
         int high=n-1;
         while(low<=high){
