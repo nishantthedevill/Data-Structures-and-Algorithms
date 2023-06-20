09d79b71a52ed75e5a6053c71088a74c0793741d
Nishant Raj <112903677+nishantthedevill@users.noreply.github.com>
2023-06-20 13:39:30 +0530

    Added solution - LeetHub

--- Matchsticks game - GFG/matchsticks-game.cpp
+++ Matchsticks game - GFG/matchsticks-game.cpp
@@ -7,10 +7,10 @@ class Solution {
   public:
     int matchGame(long long N) {
         // code here
-        if(N%5 == 0){
-            return -1;
+        if(N%5 != 0){
+            return N%5;
         }
-        return N%5;
+        return -1;
     }
 };
 
