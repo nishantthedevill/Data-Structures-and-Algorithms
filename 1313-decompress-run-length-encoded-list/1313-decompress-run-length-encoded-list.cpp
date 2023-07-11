b3d42ef514a25fe217bd96d08c892c59630a5bfa
Nishant Raj <112903677+nishantthedevill@users.noreply.github.com>
2023-07-11 22:30:50 +0530

    Time: 8 ms (50.18%), Space: 9.8 MB (93.44%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i+1<nums.size()-i;i++){
             int freq = nums[2*i];
             int val = nums[2*i+1];
-            while(freq--)
+            for(int j=0;j<freq;j++)
                 arr.push_back(val);
         }
         return arr;
