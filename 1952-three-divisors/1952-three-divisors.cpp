bb1baabb702a8549232a7effc84fe03819d470d1
Nishant Raj <112903677+nishantthedevill@users.noreply.github.com>
2023-07-19 17:08:31 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.54%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isThree(int n) {
+      int count = 0;
+        int i =1;
+        while(i<=n){
+            if(n%i==0)
+                count++;
+            i++;
+        }
+        if(count==3)
+            return true;
+        
+        return false;
+    }
+};
